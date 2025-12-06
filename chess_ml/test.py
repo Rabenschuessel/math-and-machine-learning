a23e11f7bb46b67a2a88ee399be0f20f2b4f7a00
Jakob Lambert-Hartmann <jakob.lamberthartmann.canada@gmail.com>
2025-12-06 15:48:04 +0100

    concatenated reward dataset for analysis

--- chess_ml/test.py
+++ chess_ml/test.py
@@ -2,24 +2,16 @@ import xarray as xr
 import torch 
 from chess_ml.env.Environment import Environment
 from chess_ml.env import Rewards 
-
-env = Environment(Rewards.ALL)
-
-
-
-t  = torch.randn(8, 11, 3)
-ds = xr.DataArray(
-    t.cpu().numpy(), 
-    dims=['game', 'time', 'reward'], 
-    coords={ 'reward': [r.__name__ for r in env._rewards] }
-)
+from pathlib import Path 
 
 
 
-ds.sum(dim=("game", "time"))
+experiment = 'trained_logs'
+log_dir    = Path("logs/rl/experiment-{}".format(experiment))
+ds         = xr.open_dataset(log_dir / 'rewards.nc')
 
 
-[0.5, 1]
-gamma = 0.9
 
-[1.4, 1]
+
+
+
--- chess_ml/train/reinforcement.py
+++ chess_ml/train/reinforcement.py
@@ -17,6 +17,49 @@ from chess_ml.model.ResBlock import ChessResBlock
 
 
 
+def log_batch(path, envs, rewards_white, rewards_black, batch_nr): 
+    # Logging reward values
+    tqdm.write("reward order: {}".format([r.__name__ for r in envs[0]._rewards]))
+    tqdm.write("mean white reward values: {}".format(rewards_white.abs().sum(dim=(0,1))))
+    tqdm.write("mean black reward values: {}".format(rewards_black.abs().sum(dim=(0,1))))
+
+    # Saving white rewards
+    path = Path(path) / "games" / "batch-{:04d}".format(batch_nr)
+    path.mkdir(parents=True, exist_ok=True)
+    # xr.DataArray(
+    #     rewards_white.cpu().numpy(), 
+    #     dims=['game', 'time', 'reward'], 
+    #     coords={ 'reward': [r.__name__ for r in envs[0]._rewards] }
+    # ).to_netcdf(path / "rewards_white.nc")
+    #
+    # xr.DataArray(
+    #     rewards_black.cpu().numpy(), 
+    #     dims=['game', 'time', 'reward'], 
+    #     coords={ 'reward': [r.__name__ for r in envs[0]._rewards] }
+    # ).to_netcdf(path / "rewards_black.nc")
+
+    (xr.concat([ 
+        xr.Dataset(
+            data_vars={
+                r.__name__: (["game", "turn"],t.cpu().numpy()[:,:,i])
+                for i, r in enumerate(envs[0]._rewards)
+            }, 
+            coords=dict(
+                game=("game", range(t.cpu().numpy().shape[0])),
+                turn=("turn", range(t.cpu().numpy().shape[1])),
+            )
+        )
+        for t in [rewards_black, rewards_white]], dim='color', join='outer')
+     .assign_coords(color=[chess.BLACK, chess.WHITE])
+     .to_netcdf(path / "rewards.nc"))
+
+
+    # Saving games as pgn
+    for gamenr, env in enumerate(envs): 
+        game = env.get_game()
+        with open(path / "game-{:06d}.pgn".format(gamenr), "w") as f:
+            print(game, file=f)
+
 
 
 def train_batch(model, optim, envs, log_dir, batch_nr, gamma):
@@ -45,59 +88,30 @@ def train_batch(model, optim, envs, log_dir, batch_nr, gamma):
             color = not color 
             pbar.update(sum(done) - pbar.n)
 
+    # transform to torch tensors
     rewards_white, rewards_black = zip(*[env.get_rewards() for env in envs])
-
     rewards_white   = torch.tensor(rewards_white)
     log_probs_white = torch.stack(log_probs_white)
     done_white      = torch.stack(done_white)
-
     rewards_black   = torch.tensor(rewards_black)
     log_probs_black = torch.stack(log_probs_black)
     done_black      = torch.stack(done_black)
 
-    # Logging reward values
-    tqdm.write("reward order: {}".format([r.__name__ for r in envs[0]._rewards]))
-    tqdm.write("mean white reward values: {}".format(rewards_white.abs().sum(dim=(0,1))))
-    tqdm.write("mean black reward values: {}".format(rewards_black.abs().sum(dim=(0,1))))
+    log_batch(log_dir, envs, rewards_white, rewards_black, batch_nr)
 
-    # Saving white rewards
-    path = Path(log_dir) / "games" / "batch-{}".format(batch_nr)
-    path.mkdir(parents=True, exist_ok=True)
-    ds = xr.DataArray(
-        rewards_white.cpu().numpy(), 
-        dims=['game', 'time', 'reward'], 
-        coords={ 'reward': [r.__name__ for r in envs[0]._rewards] }
-    )
-    ds.to_netcdf(path / "rewards_white.nc")
-
-    # Saving black rewards
-    ds = xr.DataArray(
-        rewards_black.cpu().numpy(), 
-        dims=['game', 'time', 'reward'], 
-        coords={ 'reward': [r.__name__ for r in envs[0]._rewards] }
-    )
-    ds.to_netcdf(path / "rewards_black.nc")
-
+    # compute loss
     rewards_white = rewards_white.sum(dim=-1).permute(1, 0)
     rewards_black = rewards_black.sum(dim=-1).permute(1, 0)
-
     rewards_white = reward2go(rewards_white, done_white, gamma)
     rewards_black = reward2go(rewards_black, done_black, gamma)
+    loss_white    = (- rewards_white * log_probs_white).sum()
+    loss_black    = (- rewards_black * log_probs_black).sum()
+    loss          = loss_white + loss_black
 
-    loss_white = (- rewards_white * log_probs_white).sum()
-    loss_black = (- rewards_black * log_probs_black).sum()
-    loss = loss_white + loss_black
-
+    # optimize
     optim.zero_grad()
     loss.backward()
     optim.step()
-
-    # Saving games as pgn
-    for gamenr, env in enumerate(envs): 
-        game = env.get_game()
-        with open(path / "game-{:06d}.pgn".format(gamenr), "w") as f:
-            print(game, file=f)
-
 
     tqdm.write("loss: {}".format(loss.item()))
     tqdm.write(str(Counter([env._board.result() for env in envs])))
@@ -117,6 +131,13 @@ def train(model, optim, batches, batch_size, env_params, log_dir, gamma):
             tqdm.write("Save Checkpoint")
             torch.save(model.state_dict(), models_dir / f"checkpoint-{batch}.pth")
 
+
+    ds = [xr.open_dataset(entry / 'rewards.nc') 
+            for entry in (log_dir/"games").iterdir() 
+            if entry.is_dir() and 'batch' in entry.name]
+    ds = xr.concat(ds, dim='batch', join='outer')
+    ds = ds.assign_coords(batch=range(ds.sizes['batch']))
+    ds.to_netcdf(log_dir / "rewards.nc")
 
 
 
@@ -141,6 +162,8 @@ def main(model_path, experiment, batches, batch_size, gamma):
 
     optim = torch.optim.Adam(model.parameters())
     train(model, optim, batches, batch_size, env_params, log_dir, gamma)
+
+
 
 
 
