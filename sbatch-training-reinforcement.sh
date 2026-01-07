44d7b9ce752d12165689475bb02ae706ebce8825
Just Me <just@me.com>
2026-01-07 13:09:07 +0100

    change train reinforcement

--- sbatch-training-reinforcement.sh
+++ sbatch-training-reinforcement.sh
@@ -14,10 +14,12 @@ module load Anaconda3
 eval "$(conda shell.bash hook)"
 conda activate chess_ml
 
+# Change to the script directory
+cd "$(dirname "$0")"
 
 # Generate unique experiment name using SLURM job ID
 EXPERIMENT_NAME="job-${SLURM_JOB_ID}"
 
 # start reinforcement learning with unique folder per submission
 python -m chess_ml.train.reinforcement "$@" \
-	--experiment "${EXPERIMENT_NAME}" 
+	--experiment "${EXPERIMENT_NAME}"
