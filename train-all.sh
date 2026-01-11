e49aea2c6269b715cb52483b3bca966469484a03
Jakob Lambert-Hartmann <jakob.lamberthartmann.canada@gmail.com>
2026-01-11 22:49:38 +0100

    typo

--- train-all.sh
+++ train-all.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-
 # reinforcement model pretrained on puzzles
-imitation_jib=$(sbatch --parsable sbatch/imitation-training.sh
-	-n puzzles
-	-a resnet
-)
+imitation_jib=$(sbatch --parsable sbatch/imitation-training.sh \
+	-n puzzles \ 
+	-a resnet)
 sbatch --dependency=afterok:$imitation_jib sbatch/reinforcement-training.sh \
 	-m logs/im/experiment-puzzles/models/checkpoint-best.pth \
-	-n resnet-pretrained-win
-	-a resnet
+	-n resnet-pretrained-win \
+	-a resnet \
 	-r win
 
 # reinforcement learning with newly initialized model
 sbatch sbatch/reinforcement-training.sh \
 	-n resnet-untrained-win \ 
-	-a resnet
+	-a resnet \ 
 	-r win
